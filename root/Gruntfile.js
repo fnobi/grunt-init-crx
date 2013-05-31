3c4c00018f8c2ad6a4e7a66244a5caf48e6a864c
Fujisawa Shin <fujisawa-shin@in.kayac.com>
2013-05-31 19:44:08 +0900

    compass用の設定を追加。

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -1,15 +1,38 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            css: {
+                files: 'sass/*.scss',
+                tasks: ['compass:dev']
+            }
+        },
+        compass: {
+            dist: {
+                options: {
+                    config: 'config.rb',
+                    environment: 'production'
+                }
+            },
+            dev: {
+                options: {
+                    config: 'config.rb',
+                    environment: 'development'
+                }
+            }
+        },
         koko: {
             dev: {
-                openPath: '/'
+                openPath: '/popup.html'
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-koko');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
 
     grunt.registerTask('server', ['koko:dev']);
-    grunt.registerTask('default', ['server']);
+    grunt.registerTask('build', ['compass:dev']);
+    grunt.registerTask('default', ['build']);
 };
